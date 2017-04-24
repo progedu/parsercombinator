26a983c9140c4b651d3f5eaa1456c192bf4c998a
murosan <masashi_16n1200013@nnn.ed.jp>
2017-04-24 16:40:57 +0900

    挑戦の中級・上級

---
+++ src/main/scala-2.11/jp/ed/nnn/parsercombinator/NaturalNumberArithParser.scala
@@ -0,0 +1,23 @@
+package jp.ed.nnn.parsercombinator
+
+case class NaturalNumberArith(num: Int, terms: List[(String, Int)])
+
+object NaturalNumberArithParser extends MyFirstCombinator {
+
+  def digitExcludingZero: Parser[String] = oneOf('1' to '9')
+
+  def digit: Parser[String] = oneOf(Seq('0')) match {
+    case zero: Parser[String] => zero
+    case _ => digitExcludingZero
+  }
+
+  def naturalNumber: Parser[Int] = map(combine(digitExcludingZero, rep(digitExcludingZero)), {
+    t: (String, List[String]) => (t._1 + t._2.mkString).toInt
+  })
+
+  def apply(input: String): ParseResult[NaturalNumberArith] = map(
+    combine(naturalNumber, rep(combine(select(oneOf(Seq('+')), oneOf(Seq('-'))), naturalNumber))), {
+      t: (Int, List[(String, Int)]) => NaturalNumberArith(t._1, t._2)
+    })(input)
+
+}
