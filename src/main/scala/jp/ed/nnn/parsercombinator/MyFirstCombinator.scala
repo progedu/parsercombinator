9dda6c8522ba844bdf553946e72773da802c224a
Taiyo Utsuhara <33287507+taiyoutsuhara@users.noreply.github.com>
2019-06-08 21:49:19 +0900

    webapp-curriculum-2011課題

--- src/main/scala/jp/ed/nnn/parsercombinator/MyFirstCombinator.scala
+++ src/main/scala/jp/ed/nnn/parsercombinator/MyFirstCombinator.scala
@@ -3,13 +3,15 @@ package jp.ed.nnn.parsercombinator
 abstract class MyFirstCombinator {
 
   sealed trait ParseResult[+T]
+
   case class Success[+T](value: T, next: String) extends ParseResult[T]
+
   case object Failure extends ParseResult[Nothing]
 
   type Parser[+T] = String => ParseResult[T]
 
   def string(literal: String): Parser[String] = input => {
-    if(input.startsWith(literal)) {
+    if (input.startsWith(literal)) {
       Success(literal, input.substring(literal.length))
     } else {
       Failure
@@ -18,13 +20,14 @@ abstract class MyFirstCombinator {
 
   /**
     * string parser
+    *
     * @param literal 文字列
     * @return
     */
   def s(literal: String): Parser[String] = string(literal)
 
   def oneOf(chars: Seq[Char]): Parser[String] = input => {
-    if(input.length != 0 &&
+    if (input.length != 0 &&
       chars.contains(input.head)) {
       Success(input.head.toString, input.tail)
     } else {
@@ -32,14 +35,14 @@ abstract class MyFirstCombinator {
     }
   }
 
-  def select[T, U >: T](left: Parser[T], right: Parser[U]): Parser[U] = input => {
+  def select[T, U >: T](left: => Parser[T], right: => Parser[U]): Parser[U] = input => {
     left(input) match {
       case success@Success(_, _) => success
       case Failure => right(input)
     }
   }
 
-  def combine[T, U](left: Parser[T], right: Parser[U]) : Parser[(T, U)] = input => {
+  def combine[T, U](left: Parser[T], right: Parser[U]): Parser[(T, U)] = input => {
     left(input) match {
       case Success(value1, next1) =>
         right(next1) match {
@@ -58,6 +61,19 @@ abstract class MyFirstCombinator {
       case Success(value, next) => Success(function(value), next)
       case Failure => Failure
     }
+  }
+
+  def rep[T](parser: Parser[T]): Parser[List[T]] = input => {
+    def repeatRec(input: String): (List[T], String) = parser(input) match {
+      case Success(value, next1) =>
+        val (result, next2) = repeatRec(next1)
+        (value :: result, next2)
+      case Failure =>
+        (Nil, input)
+    }
+
+    val (result, next) = repeatRec(input)
+    Success(result, next)
   }
 
 }
