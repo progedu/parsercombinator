7cc02eaee29d50ec032767aa2b72cce719e39ae5
Taiyo Utsuhara <33287507+taiyoutsuhara@users.noreply.github.com>
2019-06-09 09:57:00 +0900

    fix typo

--- src/main/scala/jp/ed/nnn/parsercombinator/ArithParser.scala
+++ src/main/scala/jp/ed/nnn/parsercombinator/ArithParser.scala
@@ -1,6 +1,6 @@
 package jp.ed.nnn.parsercombinator
 
-class ArithParser extends Combinator {
+object ArithParser extends Combinator {
   def expr: Parser[Any] = term ~ rep(s("+") ~ term | s("-") ~ term)
 
   def term: Parser[Any] = factor ~ rep(s("*") ~ factor | s("/") ~ factor)
